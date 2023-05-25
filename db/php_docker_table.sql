6d04537baabca1e61b2823bf50d17bfdf1a1c49a
safaa-mammed <safaa.mammed@gmail.com>
2023-05-25 18:01:16 +0800

    change1

---
+++ db/php_docker_table.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: db:3306
+-- Generation Time: May 25, 2023 at 09:45 AM
+-- Server version: 8.0.33
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `php_docker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `php_docker_table`
+--
+
+CREATE TABLE `php_docker_table` (
+  `id` int NOT NULL,
+  `title` varchar(200) NOT NULL,
+  `text` text NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `php_docker_table`
+--
+
+INSERT INTO `php_docker_table` (`id`, `title`, `text`, `date_created`) VALUES
+(1, 'First post', 'first post body', '2023-05-01'),
+(2, 'Second post', 'Seconf post body', '2023-05-05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `php_docker_table`
+--
+ALTER TABLE `php_docker_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `php_docker_table`
+--
+ALTER TABLE `php_docker_table`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
